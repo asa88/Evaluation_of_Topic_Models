f0c1e77eb71908227b79f33708f501420de767f1
ankit <ankit.10arya@gmail.com>
2014-03-30 21:12:43 -0400

    plots added

--- matlabCode/logistReg.m
+++ matlabCode/logistReg.m
@@ -1,8 +1,8 @@
 clear;
 clc;
 %addpath('/home/ankit/software/liblinear-1.93/');
-%load 'apPressData.mat'   % AP Press Datatset
-load 'econData.mat'   % Economics Datatset
+load 'apPressData.mat'   % AP Press Datatset
+%load 'econData.mat'   % Economics Datatset
 
 %scale the features between 0 and 1
 I=Features;
@@ -15,27 +15,41 @@ sprintf('Done Scaling Features')
 
 X=Features;
 Y=labels;
-reg_c=1.3;
-inc=0.0001
+reg_c=0.001;
+inc=5
 best_c=0; max_model=0;
-for iter=1:100
-	options=sprintf('-s %d -c %d -v %d -q',5,reg_c,75); %liblinear options
-	model = train(Y,sparse(X), options);    %liblinear call
-	%options=sprintf('-s %d -c %d -v %d -q',0,reg_c,75); %libsvm options
-	%model = svmtrain(Y,X, options);     %libsvm call
+cv=[]; c_val=[];
+
+for iter=-15:10,	
+	reg_c=2^iter;
+	%LIBLINEAR
+	%---------------------------------------------------------------------------------------
+	%options=sprintf('-s %d -c %d -v %d -q',6,reg_c,75); %liblinear  crossvalidation options
+	
+	%model = train(Y,sparse(X), options);    %liblinear call
+	%LIBSVM
+	%---------------------------------------------------------------------------------------
+	options=sprintf('-s %d -c %d -v %d -q',0,reg_c,75); %libsvm options
+	model = svmtrain(Y,X, options);     %libsvm call
+	
 	if max_model<model,
 		best_c=reg_c;
 		max_model=model;
 	end
-	reg_c=reg_c+inc;
-	plot(iter,100-model,'LineWidth',2,'MarkerSize',10,'Marker','*');
-	drawnow;
-	hold on;
+	cv=[cv;model];
+	c_val=[c_val;reg_c];
+	%reg_c=reg_c+inc;
+	
+
 end
-
-
-
-
+	figure;
+	plot(c_val,100-cv,'LineWidth',1);
+	axis([min(c_val) max(c_val) 0 100])
+	title('Parameter Selection through cross-validation');
+	xlabel('value of C (regualriztion parameter)');
+	ylabel('Cross-validation error');
+	
+	
 
 
 
---
+++ matlabCode/logistRegClassify.m
@@ -0,0 +1,49 @@
+clear;
+clc;
+%addpath('/home/ankit/software/liblinear-1.93/');
+%load('apPressData.mat');   % AP Press Datatset
+Ts=load('apPressData.mat');
+load('econData.mat');   % Economics Datatset
+%Ts=load('econData.mat'); 
+
+%scale the Training features between 0 and 1
+I=Features;
+for i=1:size(Features,2),
+	scaledI = (I(:,i)-min(I(:,i))) ./ (max(I(:,i)-min(I(:,i))));
+	Features(:,i)=scaledI;
+end
+
+%scale the Testing features between 0 and 1
+I=Ts.Features;
+for i=1:size(Ts.Features,2),
+	scaledI = (I(:,i)-min(I(:,i))) ./ (max(I(:,i)-min(I(:,i))));
+	Ts.Features(:,i)=scaledI;
+end
+
+sprintf('Done Scaling Features')
+
+X=Features;
+Y=labels;
+ts_data=Ts.Features;
+ts_labels=Ts.labels;
+c=65;
+classifier=5;
+best_c=0; max_model=0;
+
+
+	%LIBLINEAR
+	%---------------------------------------------------------------------------------------
+	%options=sprintf('-s %d -c %d -v %d -q',5,reg_c,75); %liblinear  crossvalidation options
+	%options=sprintf('-s %d -c %d -q',classifier,c);   %liblinear  training options
+
+	%model = train(Y,sparse(X), options);    %liblinear call
+	%[predicted_label, accuracy, prob_estimates] = predict(ts_labels, sparse(ts_data), model,'-b 1');
+	
+	%LIBSVM
+	%---------------------------------------------------------------------------------------
+	options=sprintf('-s %d -c %d -q',0,c); %libsvm options
+	model = svmtrain(Y,X, options);     %libsvm call
+	[predict_label, accuracy, prob_estimates] = svmpredict(ts_labels, ts_data, model);
+	
+	
+	
